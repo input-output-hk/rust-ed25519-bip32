87d59f5e7687b98e6efd8965b53c06d092fc5824
goncalo-frade-iohk <goncalo.frade@iohk.io>
2025-06-11 13:17:26 +0100

    feat: add pub key derivation

--- wrapper/src/wrapper.rs
+++ wrapper/src/wrapper.rs
@@ -1,5 +1,5 @@
 use std::{collections::HashMap, convert::TryInto};
-use ed25519_bip32::{DerivationScheme, XPrv};
+use ed25519_bip32::{DerivationError, DerivationScheme, XPrv, XPub};
 
 fn xprv_to_hashmap(xprv: XPrv) -> HashMap<String, Vec<u8>> {
   let sk_encoded = xprv.extended_secret_key().to_vec();
@@ -7,6 +7,16 @@ fn xprv_to_hashmap(xprv: XPrv) -> HashMap<String, Vec<u8>> {
   
   return HashMap::from([
     ("secret_key".to_string(), sk_encoded),
+    ("chain_code".to_string(), cc_encoded),
+  ]);
+}
+
+fn xpub_to_hashmap(xpub: XPub) -> HashMap<String, Vec<u8>> {
+  let pk_encoded = xpub.public_key().to_vec();
+  let cc_encoded = xpub.chain_code().to_vec();
+  
+  return HashMap::from([
+    ("public_key".to_string(), pk_encoded),
     ("chain_code".to_string(), cc_encoded),
   ]);
 }
@@ -33,4 +43,18 @@ pub fn derive_bytes(
   let derived = xprv.derive(DerivationScheme::V2, index);
   
   return xprv_to_hashmap(derived);
+}
+
+pub fn derive_bytes_pub(
+  pk: Vec<u8>,
+  chain_code: Vec<u8>,
+  index: u32
+) -> Result<HashMap<String, Vec<u8>>, DerivationError> {
+  let pk_bytes: [u8; 32] = pk.as_slice().try_into().expect("Invalid public key length");
+  let cc_bytes: [u8; 32] = chain_code.as_slice().try_into().expect("Invalid chain code length");
+
+  let xpub = XPub::from_pk_and_chaincode(&pk_bytes, &cc_bytes);
+  let derived = xpub.derive(DerivationScheme::V2, index)?;
+
+  Ok(xpub_to_hashmap(derived))
 }
